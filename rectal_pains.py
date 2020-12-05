14ef059532bc7e435529d861df28c2be5e550156
Alison Chen <alischen@amazon.com>
2020-12-05 01:01:44 -0500

    fifth day

--- rectal_pains.py
+++ rectal_pains.py
@@ -23,7 +23,7 @@ def find_product_of_two_nums():
     return -1
 
 '''
-Day 2
+Day 2 part 1
 '''
 def find_bad_passwords():
     lines = []
@@ -44,7 +44,7 @@ def find_bad_passwords():
     return finalcount
 
 '''
-Day 2
+Day 2 part 2
 '''
 def find_bad_passwords_again():
     lines = []
@@ -65,3 +65,86 @@ def find_bad_passwords_again():
             if count == 1:
                 finalcount += 1
     return finalcount
+
+    '''
+    Day 3 part 1
+    '''
+def read_input_toboggan_sledding():
+    matrix = []
+
+    with open("day3_input.txt") as f:
+        for line in f.readlines():
+            matrix.append(line.strip())
+    return matrix
+
+def toboggan_sledding():
+    matrix = read_input_toboggan_sledding()
+
+'''
+Day 5 part 1
+'''
+def airplane_tickets_part_1():
+    highestId = 0
+    with open("day5_input.txt") as file:
+        for line in file.readlines():
+            front_back = line.strip()[:-3]
+            left_right = line.strip()[-3:]
+
+            fb_lo = 0
+            fb_hi = 127
+            for c in front_back:
+                fb_med = (fb_lo + fb_hi) // 2
+                if c == 'F':
+                    fb_hi = fb_med
+                if c == 'B':
+                    fb_lo = fb_med + 1
+            lr_lo = 0
+            lr_hi = 7
+            for c in left_right:
+                lr_med = (lr_lo + lr_hi) // 2
+                if c == 'L':
+                    lr_hi = lr_med
+                if c == 'R':
+                    lr_lo = lr_med + 1
+            highestId = max(highestId, (fb_lo * 8) + lr_lo)
+    print(highestId)
+
+'''
+Day 5 part 2
+'''
+def airplane_tickets_part_2():
+    mylist = []
+    highestId = 0
+    with open("day5_input.txt") as file:
+        for line in file.readlines():
+            front_back = line.strip()[:-3]
+            left_right = line.strip()[-3:]
+
+            fb_lo = 0
+            fb_hi = 127
+            for c in front_back:
+                fb_med = (fb_lo + fb_hi) // 2
+                if c == 'F':
+                    fb_hi = fb_med
+                if c == 'B':
+                    fb_lo = fb_med + 1
+            lr_lo = 0
+            lr_hi = 7
+            for c in left_right:
+                lr_med = (lr_lo + lr_hi) // 2
+                if c == 'L':
+                    lr_hi = lr_med
+                if c == 'R':
+                    lr_lo = lr_med + 1
+            myId = (fb_hi * 8) + lr_hi
+            mylist.append(myId)
+    mylist.sort()
+    prevnum = mylist[0]
+    for num in mylist[1:]:
+        if num != prevnum + 1:
+            return num - 1
+        else:
+            prevnum = num
+
+
+
